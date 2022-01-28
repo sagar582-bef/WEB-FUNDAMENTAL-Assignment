1496be9f21955151983f59eae77b710926fa2de2
sagar582-bef <55870074+sagar582-bef@users.noreply.github.com>
2022-01-29 00:48:59 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,14 @@
+var noOf=prompt("please enter total number");
+// var arr=[];
+// for(var i=0;i<noOf;i++){
+//     var value=prompt("please enter a value");
+//     value=parseInt(value);
+//     arr.push(value);
+// }
+// for(var i=arr.length-1;i>=0;i--){
+//     console.log(arr[i]);
+
+// }
+for(var i=noOf;i>=1;i--){
+    console.log(i);
+}
